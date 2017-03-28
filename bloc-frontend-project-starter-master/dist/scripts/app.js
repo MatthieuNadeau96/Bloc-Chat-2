0524aaf306d80879b1ecbfe2199027d78ab762ac
Matthieu <matthieunadeau96@gmail.com>
2017-03-28 12:16:44 -0400

    user-story3

--- bloc-frontend-project-starter-master/dist/scripts/app.js
+++ bloc-frontend-project-starter-master/dist/scripts/app.js
@@ -8,10 +8,11 @@
         $stateProvider
             .state('home', {
                 url: '/',
+            controller: 'HomeCtrl as home',
                 templateUrl: '/templates/home.html'
         });
     }
     angular 
-        .module('blocChat', ['ui-router', 'firebase'])
+        .module('blocChat', ['ui.router', 'firebase', 'ui.bootstrap'])
         .config(config);
 })();
